d5b1660e87077572d403a8bdd340b764ee399883
Beoran <beoran@rubyforge.org>
2010-02-09 14:21:24 +0100

    Adding specs and helpesr for spec for the Zori gui library, to begin with.

---
+++ spec/ring_spec.rb
@@ -0,0 +1,23 @@
+
+require 'helper'
+require 'zori'
+
+
+describe Zori::Ring, "gui object" do
+  include Helper::Common
+  
+  before(:all) do
+    setup
+    @ui    = Zori.open(@screen, @queue)
+  end
+  
+  after(:all) do
+    teardown
+  end
+  
+  it "Can be contructed" do
+    ring = Zori::Ring.new(@zori, {:foo => :bar})
+  end
+
+
+end
---
+++ spec/zori_spec.rb
@@ -0,0 +1,27 @@
+require 'helper'
+
+
+describe Zori, "gui library" do
+  include Helper::Common
+  
+  before(:all) do
+    setup() 
+  end
+  
+  after(:all) do
+    teardown
+  end
+
+
+  it "should be constructable" do
+    # p Sisa::Joystick.methods.sort - Object.methods
+    ui     = Zori.open(@screen, @queue)
+    ui.should_not be_nil
+
+  end
+
+
+end
+
+
+
