8f792ad1dea6fb3e2c5213300b174b138e5160ec
Beoran <beoran@rubyforge.org>
2010-02-11 15:30:19 +0100

    Refacoring the map editor.

--- src/zori/mapeditor.rb
+++ src/zori/mapeditor.rb
@@ -13,111 +13,104 @@ module Zori
       @clock  = Rogaru::Clock.new()
       @camera = Rogaru::Tilemap::Camera.new(0, 0, 
                 @screen.w, @screen.h, *@map.map_size)
-      map.camera  = camera
+      map.camera  = @camera
+      # camera.lock_on(@mousetrack) # Let the camera of the map follow the mouse
+      @edit_bg    = nil
+      @edit_x     = 0
+      @edit_y     = 0 
+      @edit_layer = 0 
+      @edit_select= 0
+      @edit_tile  = 0
+      @tilewide   = 32
     end
 
-    camera.lock_on(@mousetrack) # Let the camera of the map follow the mouse
+    def draw(screen)
+      screen.fill_rect(0,0,640,480, Sisa::Color::Black) if edit_bg
+      @map.draw_auto_with_effects(screen)
+      mark_active_tile(screen)
+      mark_active_selection(screen)
+      status.text  = "XYZ: #{@edit_x}, #{@edit_y}," + 
+                     "#{@edit_layer}/#{@map.layer_count-1}" +  
+                     ". Tile: #{@edit_tile}. Select: #{@edit_select}"
+    end
     
-    until done do
-      screen.fill_rect(0,0,640,480, screen.format.map_rgb(255, 0, 255)) if edit_bg
-      map.draw_auto_with_effects(screen)
-      mark_active_tile(screen, map, edit_x, edit_y, edit_layer, tilewide)  
-      mark_active_selection(screen, map, edit_select, edit_layer, tilewide)
-      status.text  = "#{filename} Speed: #{speed}. XYZ: #{edit_x}, #{edit_y}," + 
-                     "#{edit_layer}/#{map.layer_count-1}" +  
-                     ". Tile: #{edit_tile}. Select: #{edit_select}"
-      # status.draw_all(screen)
-      gui.draw(screen)
-      screen.flip()
-      queue.each do | event | 
-        if(event.is_a? Sisa::Event::Quit) then         
-          done = Zori::Dialog.yesno('Quit without saving?')
-        elsif (event.is_a? Sisa::Event::KeyDown) then 
-          if event.sym == Sisa::Key::L
-            puts "Map new layer: #{map.tw}, #{map.th}"
-            map.new_layer(map.tw, map.th) 
-          end
-          # Add new Layer.
-          edit_select.copy(map, edit_layer) if event.sym == Sisa::Key::C
-          # Copy selection.
-          edit_select.paste(map, edit_x, edit_y, edit_layer) if event.sym == Sisa::Key::P
-          # Paste selection.
-          edit_tile = map.get(edit_x, edit_y, edit_layer) if event.sym == Sisa::Key::F5
-          # Set the active tile to the one currently under the mouse pointer
-          done = true if event.sym == Sisa::Key::F10
-          # Exit with save.
-          if event.sym == Sisa::Key::ESCAPE
-            done = Zori::Dialog.yesno('Quit without saving?')
-          end  
-          # Exit without save.
-
-          edit_bg = (not edit_bg) if event.sym == Sisa::Key::B
-          # Toggle drawing of simple bright pink background
-
-          # Toggle full screen
-          if event.sym == Sisa::Key::F12
-            fullscreen  = !fullscreen
-            screen      = screen_make(640, 480, fullscreen, true)
-            # gui         = Zori.open(screen, queue)
-          end
-
-          if event.sym == Sisa::Key::W
-            tile_i = map.get(edit_x, edit_y, edit_layer)
-            tile = map.get_tileset_tile(tile_i)
-            tile.walk = (not tile.walk)  unless tile.nil?
-          end
-          # Toggle walkability of tile under cursor and all similar tiles
-          if event.sym == Sisa::Key::S
-            tile_i = map.get(edit_x, edit_y, edit_layer)
-            tile = map.get_tileset_tile(tile_i)
-            tile.swim = (not tile.swim)  unless tile.nil?
-          end
-          # Toggle swimmable property of tile
-          if event.sym == Sisa::Key::J
-            tile_i = map.get(edit_x, edit_y, edit_layer)
-            tile = map.get_tileset_tile(tile_i)
-            tile.jump = (not tile.jump)  unless tile.nil?
-          end
-          # Toggle jump property of tile
-          if event.sym == Sisa::Key::U
-            tile_i = map.get(edit_x, edit_y, edit_layer)
-            tile = map.get_tileset_tile(tile_i)
-            tile.up = (not tile.up)  unless tile.nil?
-          end
-          # Toggle Up property of tile
-          if event.sym == Sisa::Key::D
-            tile_i = map.get(edit_x, edit_y, edit_layer)
-            tile = map.get_tileset_tile(tile_i)
-            tile.down = (not tile.down)  unless tile.nil?
-          end
-          # Toggle Down property of tile
-          if event.sym == Sisa::Key::O
-            tile_i = map.get(edit_x, edit_y, edit_layer)
-            tile = map.get_tileset_tile(tile_i)
-            tile.pain = (not tile.pain)  unless tile.nil?
-          end
-          # Toggle Ouch property of tile, that is pain
-          if event.sym == Sisa::Key::E
-            tile_i = map.get(edit_x, edit_y, edit_layer)
-            tile = map.get_tileset_tile(tile_i)
-            tile.ledge = (not tile.ledge) unless tile.nil?
-          end
-          # Toggle Edge/Ledge property of tile
-           speed += 5 if event.sym == Sisa::Key::KP_MULTIPLY and speed < 100
-          # Speed of scrolling up
-           speed -= 5 if event.sym == Sisa::Key::KP_DIVIDE and speed > 5
-          # Speed of scrolling down
-          edit_layer += 1 if event.sym == Sisa::Key::KP_PLUS and edit_layer  < (map.layer_count-1)
-          # Up to next layer
-          edit_layer -= 1 if event.sym == Sisa::Key::KP_MINUS and edit_layer > 0
-          # Down to previous layer
-          if event.sym == Sisa::Key::F2  or event.sym == Sisa::Key::F10 then
-          # Save map
-            Zori::Dialog.textsplash("Saving to #{filename}...", 1.0)
+    def on_quit(*args)
+      done = Zori::Dialog.yesno('Quit without saving?')
+    end
+      
+    def on_key(sym, mod, text = nil)
+      select sym 
+      case Sisa::Key::L      # Add new Layer.
+        puts "Map new layer: #{map.tw}, #{map.th}"
+        map.new_layer(@map.tw, @map.th) 
+      case Sisa::Key::C      # Copy selection.        
+        @edit_select.copy(@map, @edit_layer)
+      case  Sisa::Key::P # Paste selection. 
+        @edit_select.paste(@map, @edit_x, @edit_y, @edit_layer)
+      # Set the active tile to the one currently under the mouse pointer 
+      case Sisa::Key::F5
+        @edit_tile = map.get(@edit_x, @edit_y, @edit_layer)  
+      case Sisa::Key::F10
+        @done = true 
+      case Sisa::Key::ESCAPE # Exit without save.
+        done = Zori::Dialog.yesno('Quit without saving?')
+      # Toggle drawing of simple bright pink background
+      case Sisa::Key::B
+        @edit_bg = (not @edit_bg)
+      case Sisa::Key::F12
+        @fullscreen  = !@fullscreen
+        @screen.fullscreen = @fullscreen
+      # Toggle walkability of tile under cursor and all similar tiles        
+      case Sisa::Key::W
+        tile_i = map.get(edit_x, edit_y, edit_layer)
+        tile = map.get_tileset_tile(tile_i)
+        tile.walk = (not tile.walk)  unless tile.nil?
+      # Toggle swimmable property of tile  
+      case Sisa::Key::S
+        tile_i = map.get(edit_x, edit_y, edit_layer)
+        tile = map.get_tileset_tile(tile_i)
+        tile.swim = (not tile.swim)  unless tile.nil?
+      case Sisa::Key::J
+        tile_i = map.get(edit_x, edit_y, edit_layer)
+        tile = map.get_tileset_tile(tile_i)
+        tile.jump = (not tile.jump)  unless tile.nil?
+      case Sisa::Key::U
+        tile_i = map.get(edit_x, edit_y, edit_layer)
+        tile = map.get_tileset_tile(tile_i)
+        tile.up = (not tile.up)  unless tile.nil?
+      case Sisa::Key::D
+        tile_i = map.get(edit_x, edit_y, edit_layer)
+        tile = map.get_tileset_tile(tile_i)
+        tile.down = (not tile.down)  unless tile.nil?
+      case Sisa::Key::O
+        tile_i = map.get(edit_x, edit_y, edit_layer)
+        tile = map.get_tileset_tile(tile_i)
+        tile.pain = (not tile.pain)  unless tile.nil?
+      case Sisa::Key::E
+        tile_i = map.get(edit_x, edit_y, edit_layer)
+        tile = map.get_tileset_tile(tile_i)
+        tile.ledge = (not tile.ledge) unless tile.nil?
+        # Speed of scrolling up
+      case Sisa::Key::KP_MULTIPLY
+        @speed += 5 if @speed < 100
+      case Sisa::Key::KP_DIVIDE
+        @speed -= 5 if @speed > 5
+      # Up to next layer        
+      case Sisa::Key::KP_PLUS
+        @edit_layer += 1 if @edit_layer  < (map.layer_count-1)
+      # Down to previous layer   
+      case Sisa::Key::KP_MINUS   
+        @edit_layer -= 1 if @edit_layer  > 0
+      case Sisa::Key::F2, Sisa::Key::F10
+      # Save map
+          Zori::Dialog.textsplash("Saving to #{filename}...", 1.0)
             screen.flip()
             map.save_to(filename)
             Zori::Dialog.textsplash("Saved to #{filename}", 2.0)
-          end
+      end
+   end
+     
+   def mouse_scroll(*args)
         elsif (event.is_a? Sisa::Event::MouseButtonDown) then 
           # Handle mouse button roll up.
           if event.button == 5 and edit_tile < map.tileset.last_index
