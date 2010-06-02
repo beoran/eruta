834128c02f53240b1f3c8fa0669a9ddce9c15069
Bjorn <bjorn@koguma.(none)>
2010-06-03 01:27:50 +0200

    sound is fine too, just a stella bug if buffer too small

--- tool/ature/bB.asm
+++ tool/ature/bB.asm
@@ -5,17 +5,125 @@ game
 .
  ; 
 
-.L00 ;  rem ' Musinum
+.L00 ;  rem ' Ature Adventure
 
-.L01 ;  rem ' Implementation of the music in numbers algorythm of Lars Kindermann 
+.L01 ;  rem ' Copyright beoran@rubyforge.org 2009, 2010.
 
-.L02 ;  rem ' for ATARI 2600
+.L02 ;  rem ' May be distributed under the Zlib License. No warranty, use at your own risk.
 
-.L03 ;  rem ' This basic file must be pulled through the C preprocessor to compile.
+.L03 ;  rem ' You will need to modify Batari Basic from sources to compile this program
 
-.L04 ;  rem ' Note definitions and length 
+.L04 ;  rem ' You'll also need to pull it through a C preprocessor like cpp -P
 
-.L05 ;  rem ' Use it under the zlib license.
+.L05 ;  rem ' Because I'm using much more constants than Batari Basic supports normally.
+
+.L06 ;  rem ' And I use the C preprocessor as a workaround to Batari Basic's
+
+.L07 ;  rem ' bugs with regards to constant definition
+
+.L08 ;  rem ' It's much easier to understand what's going on if I use constants galore.
+
+.L09 ;  rem ' Note: I only use integer math, for speed.
+
+.L010 ;  rem ' To Do: Fix some weirdness in the color handling. 
+
+.L011 ;  rem ' To Do: Fix sound effects and music. 
+
+.L012 ;  rem ' To Do: Make the game bigger. Perhaps add a few more areas?
+
+.L013 ;  rem ' We use playfield colors, 32K and smart branching.
+
+.L014 ;  set kernel_options pfcolors
+
+.L015 ;  set romsize 32k
+
+.L016 ;  set smartbranching on
+
+.L017 ;  rem ' Cycle debugging
+
+.L018 ;  rem ' set debug cycles
+
+.L019 ;  rem ' set debug cyclescore
+
+.L020 ;  rem ' dim mincycles=z
+
+.
+ ; 
+
+.L021 ;  rem ' mincycles=255
+
+.
+ ; 
+
+.L022 ;  rem 'Constant definitions
+
+.
+ ; 
+
+.L023 ;  rem 'const pfres=12
+
+.L024 ;  rem 'const pfrowheight=8
+
+.
+ ; 
+
+.L025 ;  rem 'Commonly used NTSC Colors
+
+.L026 ;  const black = 0
+
+.L027 ;  const gray = 6
+
+.L028 ;  const white = 14
+
+.L029 ;  const yellow = 30
+
+.
+ ; 
+
+.L030 ;  const brown = 32
+
+.L031 ;  const orange = 32
+
+.L032 ;  const blue = 144
+
+.L033 ;  const skyblue = 174
+
+.L034 ;  const green = 192
+
+.L035 ;  const darkgreen = 191
+
+.L036 ;  const riverblue = 170
+
+.L037 ;  rem ' XXX: there is a bug in the ompiler (or my patches to it) 
+
+.L038 ;  rem ' some of my consants like the one below are translated in stead 
+
+.L039 ;  rem ' like a dim statement: eg  const turfy=218 -> dim   turfy=218
+
+.L040 ;  rem ' a workaround seem to be to reorder the constants a bit
+
+.L041 ;  rem ' or to remove a few of them, but this only bugs out other constants :p
+
+.L042 ;  rem ' so I replaced some constants by their values, and use the cpp 
+
+.L043 ;  rem ' preprocessor in stead.    
+
+.
+ ; 
+
+.L044 ;  const seablue  =  144
+
+.L045 ;  const red  =  64
+
+.L046 ;  const pink  =  78
+
+.L047 ;  const skin  =  254
+
+.L048 ;  const sand  =  230
+
+.L049 ;  const turfy  =  218
+
+.L050 ;  rem ' const ochre=16
 
 .
  ; 
@@ -23,303 +131,322 @@ game
 .
  ; 
 
+.L051 ;  rem 'Playfield dimensions
+
+.L052 ;  const field_left = 17
+
+.L053 ;  const field_left_enter = 18
+
+.L054 ;  const field_right = $88
+
+.L055 ;  const field_right_enter = $87
+
+.L056 ;  const field_top = 10
+
+.L057 ;  const field_top_enter = 11
+
+.L058 ;  const field_bottom = 90
+
+.L059 ;  const field_bottom_enter = 89
+
+.L060 ;  const field_hcenter = 61
+
+.L061 ;  const field_vcenter = 44
+
 .
  ; 
 
-.L06 ;  rem ' The notes below are all for voice 12 
+.L062 ;  rem ' Note definitions and length const music_voice=12
 
-.L07 ;  rem 'Timer ticks per second, 60 for NTSC
+.L063 ;  rem const note_c4=30
 
-.L08 ;  rem ' Music wave form to use.
+.L064 ;  rem const note_d4=27
 
-.L09 ;  rem ' these two below will determine tthe tune that is generated
+.L065 ;  rem const note_e4=24
 
-.L010 ;  rem 'Timer and pointer for music and sound
+.L066 ;  rem const note_f4=23
 
-.L011 ;  dim music_counter = m
+.L067 ;  rem const note_g4=20
 
-.L012 ;  dim music_timer = n
+.L068 ;  rem const note_a4=18
 
-.L013 ;  dim sound_timer = p
+.L069 ;  rem const note_b4=16
 
-.L014 ;  dim music_last = z
+.L070 ;  rem const note_c5=15
 
-.musinum_reset
- ; musinum_reset
+.L071 ;  rem const note_none=0
 
-.L015 ;  rem ' Volume down and set music channels.
+.L072 ;  rem const note_full=60
 
-.L016 ;  AUDC0 = 4
+.L073 ;  rem const note_half=30
 
-	LDA #4
-	STA AUDC0
-.L017 ;  AUDV0 = 0
+.L074 ;  rem const note_quarter=15
 
-	LDA #0
-	STA AUDV0
-.L018 ;  AUDC0 = 0
+.L075 ;  rem const note_fullrest=58
 
-	LDA #0
-	STA AUDC0
-.L019 ;  rem ' Initialize music timer
+.L076 ;  rem const note_halfrest=28
 
-.L020 ;  music_counter =  101
+.L077 ;  rem const note_quarterest=13
 
-	LDA #101
-	STA music_counter
-.L021 ;  music_timer  =  0
+.L078 ;  rem const note_ndot=2
 
-	LDA #0
-	STA music_timer
-.L022 ;  music_last  =  0
+.
+ ; 
 
-	LDA #0
-	STA music_last
-.L023 ;  playfield:
+.
+ ; 
 
-  ifconst pfres
-    ldx #4*pfres-1
-  else
-	  ldx #47
-  endif
-	jmp pflabel0
-PF_data0
-	.byte %00000000, %10000000, %00000000, %00000000
-	.byte %00000000, %10000000, %11111000, %00000000
-	.byte %00000000, %10000000, %10011111, %00000000
-	.byte %00000000, %10000000, %10000011, %00000011
-	.byte %00000000, %10000000, %10000000, %00000000
-	.byte %00000000, %10000000, %10000000, %00000000
-	.byte %00000000, %10000000, %10000000, %00000000
-	.byte %00000000, %11111000, %10000000, %00000000
-	.byte %00000000, %11111100, %10000000, %00000000
-	.byte %00000000, %11111110, %10000000, %00000000
-	.byte %00000000, %11111100, %00000000, %00000000
-	.byte %00000000, %01111000, %00000000, %00000000
-	.byte %00000000, %00000000, %00000000, %00000000
-pflabel0
-	lda PF_data0,x
-	sta playfield,x
-	dex
-	bpl pflabel0
-.101
- ; 101
+.L079 ;  rem ' An object will be positioned at 255 to hide it
 
-.L024 ;  if switchreset then goto musinum_reset
+.L080 ;  const nowhere = 255
 
- lda #1
- bit SWCHB
-	BNE .skipL024
-.condpart0
- jmp .musinum_reset
+.
+ ; 
 
-.skipL024
-.L025 ;  gosub musinum_music
+.L081 ;  rem 'Timer ticks per second, 60 for NTSC
 
- jsr .musinum_music
+.L082 ;  const ticks_per_second = 60
 
-.L026 ;  rem ' Draw screen and reset
+.
+ ; 
 
-.L027 ;  drawscreen
+.L083 ;  rem 'Score, split up in individual digits
 
- jsr drawscreen
-.L028 ;  goto 101
+.L084 ;  dim sc0 = score
 
- jmp .101
+.L085 ;  dim sc1 = score + 1
 
-.L029 ;  data musinum_notes
-
-	JMP .skipL029
-musinum_notes
-	.byte     23, 20, 19, 17, 15, 14, 12, 11
-
-.skipL029
-.L030 ;  data musinum_notes_penta
-
-	JMP .skipL030
-musinum_notes_penta
-	.byte     31, 28, 23, 20, 18 
-
-	.byte     15, 13, 11
-
-.skipL030
-.musinum_music
- ; musinum_music
-
-.L031 ;  if music_timer  =  0 then goto musinum_music_timer_down_end
-
-	LDA music_timer
-	CMP #0
-     BNE .skipL031
-.condpart1
- jmp .musinum_music_timer_down_end
-
-.skipL031
-.L032 ;  rem ' Update music timer and skip note changing if
-
-.L033 ;  music_timer  =  music_timer  -  1
-
-	DEC music_timer
-.musinum_music_timer_down_end
- ; musinum_music_timer_down_end
-
-.L034 ;  if music_timer  >  1 then goto musinum_music_timer_end
-
-	LDA #1
-	CMP music_timer
-     BCS .skipL034
-.condpart2
- jmp .musinum_music_timer_end
-
-.skipL034
-.L035 ;  rem 'Change background on note change
-
-.L036 ;  rem ' Sum of the bits
-
-.L037 ;  temp1  =  0
-
-	LDA #0
-	STA temp1
-.L038 ;  if music_counter{0} then temp1  =  temp1  +  1
-
-	LDA music_counter
-	LSR
-	BCC .skipL038
-.condpart3
-	INC temp1
-.skipL038
-.L039 ;  if music_counter{1} then temp1  =  temp1  +  1
-
-	LDA music_counter
-	AND #2
-	BEQ .skipL039
-.condpart4
-	INC temp1
-.skipL039
-.L040 ;  if music_counter{2} then temp1  =  temp1  +  1
-
-	LDA music_counter
-	AND #4
-	BEQ .skipL040
-.condpart5
-	INC temp1
-.skipL040
-.L041 ;  if music_counter{3} then temp1  =  temp1  +  1
-
-	LDA music_counter
-	AND #8
-	BEQ .skipL041
-.condpart6
-	INC temp1
-.skipL041
-.L042 ;  if music_counter{4} then temp1  =  temp1  +  1
-
-	LDA music_counter
-	AND #16
-	BEQ .skipL042
-.condpart7
-	INC temp1
-.skipL042
-.L043 ;  if music_counter{5} then temp1  =  temp1  +  1
-
-	LDA music_counter
-	AND #32
-	BEQ .skipL043
-.condpart8
-	INC temp1
-.skipL043
-.L044 ;  if music_counter{6} then temp1  =  temp1  +  1
-
-	BIT music_counter
-	BVC .skipL044
-.condpart9
-	INC temp1
-.skipL044
-.L045 ;  if music_counter{7} then temp1  =  temp1  +  1
-
-	BIT music_counter
-	BPL .skipL045
-.condpart10
-	INC temp1
-.skipL045
-.L046 ;  rem 'set matching note. Sum can never be more than 8
-
-.L047 ;  temp2  =  musinum_notes_penta[temp1]
-
-	LDX temp1
-	LDA musinum_notes_penta,x
-	STA temp2
-.L048 ;  COLUBK  =  0
-
-	LDA #0
-	STA COLUBK
-.L049 ;  COLUPF  =  temp2  *  16
-
-	LDA temp2
-	asl
-	asl
-	asl
-	asl
-	STA COLUPF
-.L050 ;  music_timer  =  15
-
-	LDA #15
-	STA music_timer
-.L051 ;  AUDC0  =  4
-
-	LDA #4
-	STA AUDC0
-.L052 ;  AUDF0  =  temp2
-
-	LDA temp2
-	STA AUDF0
-.L053 ;  rem ' randomly insert a rest
-
-.L054 ;  temp3  =  rand
-
- jsr randomize
-	STA temp3
-.L055 ;  if temp3  <  240 goto musinum_setnote
-
-	LDA temp3
-	CMP #240
-	bcc .musinum_setnote
-.L056 ;  AUDV0  =  0
-
-	LDA #0
-	STA AUDV0
-.L057 ;  goto musinum_music_setnote_end
-
- jmp .musinum_music_setnote_end
-
-.musinum_setnote
- ; musinum_setnote
-
-.L058 ;  AUDV0  =  4
-
-	LDA #4
-	STA AUDV0
-.L059 ;  rem 'On to next note, allowing overflow.  
-
-.musinum_music_setnote_end
- ; musinum_music_setnote_end
-
-.L060 ;  music_counter  =  music_counter  +  2
-
-	LDA music_counter
-	CLC
-	ADC #2
-	STA music_counter
-.musinum_music_counter_end
- ; musinum_music_counter_end
-
-.musinum_music_timer_end
- ; musinum_music_timer_end
-
-.L061 ;  return
-
-	RTS
-       echo "    ",[(scoretable - *)]d , "bytes of ROM space left")
- 
- 
- 
+.L086 ;  dim sc2 = score + 2
+
+.
+ ; 
+
+.L087 ;  rem 'Initial values
+
+.L088 ;  rem 'Normal start
+
+.L089 ;  const room_start = 49
+
+.L090 ;  rem 'On the east side of the river
+
+.L091 ;  rem 'const room_start=4
+
+.
+ ; 
+
+.L092 ;  const hero_start_x = 75
+
+.L093 ;  const hero_start_y = 75
+
+.L094 ;  const item_start_x = 60
+
+.L095 ;  const item_start_y = 40
+
+.L096 ;  const item_start_hp = 1
+
+.L097 ;  const hero_base_hp = 11
+
+.L098 ;  const hero_high = 8
+
+.L099 ;  const hero_half_high = 4
+
+.L0100 ;  const hero_feet_high = 4
+
+.L0101 ;  const hero_wide = 8
+
+.L0102 ;  const hero_half_wide = 4
+
+.L0103 ;  rem 'Hero's position, speed and room
+
+.L0104 ;  dim hero_x = player0x
+
+.L0105 ;  dim hero_y = player0y
+
+.
+ ; 
+
+.L0106 ;  dim hero_oldx = v
+
+.L0107 ;  dim hero_oldy = w
+
+.L0108 ;  rem 'Flags:
+
+.L0109 ;  dim hero_flags = g
+
+.L0110 ;  rem 'hero_flags{0} -> facing north
+
+.L0111 ;  rem 'hero_flags{1} -> facing east
+
+.L0112 ;  rem 'hero_flags{2} -> facing south
+
+.L0113 ;  rem 'hero_flags{3} -> facing west
+
+.L0114 ;  rem 'hero_flags{4} -> set when attacking is allowed, blocked when not
+
+.L0115 ;  dim hero_items = q
+
+.L0116 ;  rem 'hero_items{0} -> Key Leaf 1
+
+.L0117 ;  rem 'hero_items{1} -> Key Leaf 2
+
+.L0118 ;  rem 'hero_items{2} -> Key Leaf 3
+
+.L0119 ;  rem 'hero_items{3} -> Numen Shield
+
+.L0120 ;  rem 'hero_items{4} -> Numen Armor 
+
+.L0121 ;  rem 'hero_items{5} -> Numen Sword
+
+.L0122 ;  rem 'hero_items{6} -> Heal Book
+
+.L0123 ;  rem 'hero_items{7} -> Strike Book
+
+.L0124 ;  dim hero_room = r
+
+.L0125 ;  dim hero_hp = statusbarlength
+
+.L0126 ;  dim hero_mp = o
+
+.L0127 ;  dim hero_next = k
+
+.L0128 ;  dim hero_level = l
+
+.L0129 ;  rem 'Quest flags contain important achievments of Signe.
+
+.L0130 ;  dim quest_flags = h
+
+.L0131 ;  rem 'quest_flags{0} -> Defeated Ikaza
+
+.L0132 ;  rem 'quest_flags{1} -> Flipped Switch 1
+
+.L0133 ;  rem 'quest_flags{2} -> Flipped Switch 2
+
+.L0134 ;  rem 'quest_flags{3} -> Flipped Switch 3
+
+.L0135 ;  rem 'quest_flags{4} -> Flipped Switch 4
+
+.L0136 ;  rem 'quest_flags{5} -> Flipped Switch 5
+
+.L0137 ;  rem 'quest_flags{6} -> Flipped Switch 6
+
+.L0138 ;  rem 'quest_flags{7} -> Flipped Switch 7
+
+.
+ ; 
+
+.L0139 ;  rem 'Other flags: Reserved for future extension
+
+.L0140 ;  rem 'quest_flags{1} -> Defeated Armor Boss
+
+.L0141 ;  rem 'quest_flags{2} -> Defeated Sword Boss 
+
+.L0142 ;  rem 'quest_flags{3} -> Defeated 
+
+.L0143 ;  rem 'quest_flags{0} -> Defeated Leaf Boss 1
+
+.L0144 ;  rem 'quest_flags{1} -> Defeated Leaf Boss 2
+
+.L0145 ;  rem 'quest_flags{2} -> Defeated Leaf Boss 3
+
+.L0146 ;  rem 'status bar color in Z, which si otherwise used for debugging
+
+.L0147 ;  rem ' Can't sue changes top platfield row color somehow. :p
+
+.L0148 ;  rem ' dim statusbarcolor=z
+
+.
+ ; 
+
+.L0149 ;  const strike_cost = 1
+
+.L0150 ;  const last_monster = 32
+
+.
+ ; 
+
+.L0151 ;  const hero_speed = 1
+
+.L0152 ;  const hero_speed_negative = # - 1
+
+.
+ ; 
+
+.L0153 ;  rem item/monster position speed and kind
+
+.L0154 ;  dim item_x = player1x
+
+.L0155 ;  dim item_y = player1y
+
+.L0156 ;  dim item_kind = s
+
+.L0157 ;  dim item_hp = i
+
+.L0158 ;  dim item_oldx = t
+
+.L0159 ;  dim item_oldy = u
+
+.L0160 ;  rem 'Active item flags: 
+
+.L0161 ;  dim item_flags = f
+
+.L0162 ;  rem 'item_flags{0} -> Monster missile flies north
+
+.L0163 ;  rem 'item_flags{1} -> Monster missile flies east
+
+.L0164 ;  rem 'item_flags{2} -> Monster missile flies south
+
+.L0165 ;  rem 'item_flags{3} -> Monster missile flies west
+
+.
+ ; 
+
+.
+ ; 
+
+.L0166 ;  rem 'Game timer. Controls animation. Keeps on ticking, by overflow.
+
+.L0167 ;  dim game_timer = e
+
+.L0168 ;  const timer_loop = 128
+
+.L0169 ;  const timer_second = 60
+
+.
+ ; 
+
+.
+ ; 
+
+.L0170 ;  rem 'Item and monster kinds. 
+
+.L0171 ;  rem 'Monster kind is divided as follows: the 6 bits record the looks 
+
+.L0172 ;  rem 'and the 'index' for data tables of the monster or item
+
+.L0173 ;  rem 'The high 2 bits are used for 2 flag variables: 
+
+.L0174 ;  rem 'If item_kind{7} is set it's an immobile item, otherwise, a monster
+
+.L0175 ;  rem '6 is yet to be determined
+
+.L0176 ;  const item_kind_mask = %00111111
+
+.L0177 ;  const is_item_mask = %10000000
+
+.L0178 ;  const item_none = %10000000
+
+.L0179 ;  const monster_slime = 1
+
+.L0180 ;  const monster_crab = 2
+
+.L0181 ;  const monster_bat = 3
+
+.L0182 ;  const monster_scorpio = 4
+
+.L0183 ;  rem 'The famous rabid rabbit ^_^
+
+.L0184 ;  const monster_rabid = 5
+
+.L0185 ;  const monster_spider = 6
+
+.L0186 ;  const monster_snak
