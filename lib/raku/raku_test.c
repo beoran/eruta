a94ee970ba86f6ad40ad04a1701c4a15859a1af3
Beoran <beoran@rubyforge.org>
2010-09-21 00:17:35 +0200

    Simple immutable strings and minimal memory allocation functions.

--- lib/raku/raku_test.c
+++ lib/raku/raku_test.c
@@ -2,6 +2,14 @@
 #include "raku.h"
 #include <stdio.h>
 
+TEST_FUNC(raku_mem) {
+  void * mem;
+  mem = ra_mem_allot(123);
+  ra_mem_free(mem);  
+  TEST_DONE();
+}
+
+
 TEST_FUNC(raku_string) {
   fprintf(stderr, "sizeof RaVar : %d\n", sizeof(RaVar)); 
   TEST_DONE();
@@ -12,6 +20,7 @@ int main(int argc, char * argv[]) {
   int result;
   
   TEST_INIT(); 
+  TEST_RUN(raku_mem);
   TEST_RUN(raku_string);
   TEST_REPORT();
 }
--- lib/raku/string.c
+++ lib/raku/string.c
@@ -33,6 +33,24 @@ RaString ra_string_init(RaString rope, char * data, RaSize size) {
   return rope;
 }
 
+   
+
+RaString ra_string_new(char * data, RaSize size) {
+  RaString string = ra_mem_allot(sizeof(RaString));
+  if(!string) return NULL;
+  if(!ra_string_init(string, data, size)) {
+    free(string); 
+    return NULL;
+  }
+  return string;
+}
+
+RaString ra_string_fromc(char * data) {
+  RaSize size = strlen(data);
+  return ra_string_new(data, size);
+}
+
+
 RaString ra_string_done(RaString string) {
   if(string && string->data) {
     ra_mem_free(string->data);
