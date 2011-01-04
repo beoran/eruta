111fe3a50237c75aea66ab5a37d89a2a9b6fae75
Bjorn <bjorn@koguma.(none)>
2011-01-04 01:12:10 +0100

    Added tiny graphvi vizualization lib, cleaned up from one of the tools..

---
+++ lib/nanograph.rb
@@ -0,0 +1,214 @@
+# Tiny module for handling Graphviz Module DOT graphs
+# Useful when implementing parsers to visualise your AST's.
+
+module Nanograph
+  # Helper function to process attributes
+  def self.attributes_to_dot(attributes, label = nil)
+    if (attributes.nil? || attributes.empty?) && (label.nil? || label.empty?) 
+      return ""
+    end
+    
+    res = " [ "    
+    if label && (!label.empty?)
+      res << ' ' + 'label="' + label.to_s + '"'
+    end
+    for key, value in attributes do
+      res << ' ' + key.to_s + '="' + value.to_s + '"'
+    end    
+    res << " ] \n"
+    return res
+  end  
+  
+  # Creates a new graph with the given attributes.
+  def self.graph(attrs={})
+    return Graph.new(attrs)
+  end
+
+  # Aaaaa node in a graph 
+  class Node
+    attr_accessor :attributes
+    attr_accessor :graph
+    attr_accessor :id
+    attr_accessor :label
+    
+
+    def initialize(graph, id, label = nil, attrs = {})
+      @graph      = graph
+      @id         = id
+      if label
+        @label      = label.gsub(/[\r\n]/,'\n').gsub('\\','\\\\') 
+      else 
+        @label = id.to_s
+      end
+      @attributes = attrs
+    end
+    
+    # Converts the node id to dot format.
+    def to_dot_id
+      return '  "' + id  + '"'
+    end
+    
+    # Converts the node to dot format.
+    def to_dot()
+      res         =  to_dot_id + Nanograph.attributes_to_dot(@attributes,
+@label) 
+      return res
+    end
+    
+    # Looks up the node_id as a node, or adds a new node with node_id to this 
+    # node's graph and links an edge from self to it
+    # If dir is true, it's a directed edge, otherwise it's an undirected edge.
+    def edge(node_id, dir = true)
+      othernode = node_id
+      unless node_id.is_a? ::Nanograph::Node
+        othernode = @graph.new_node(othernode.to_s)
+      end
+      @graph.new_edge(self, othernode, dir)
+      return othernode 
+    end
+    
+    # Syntactic sygar for self.graph.egde(self, othernode, true )
+    # Returns othernode for chaining
+    def >> (othernode) 
+      return self.edge(othernode, true) 
+    end
+    
+    # Syntactic sygar for self.graph.egde(self, othernode, false )
+    # Returns othernode for chaining
+    def - (othernode) 
+      return self.edge(othernode, false)
+    end
+    
+  end
+  
+  # Models an edge between two graph nodes.
+  class Edge 
+   
+    attr_accessor :attributes
+    attr_accessor :graph
+    attr_accessor :from
+    attr_accessor :to
+    attr_accessor :directed
+    
+    # Initializes the Edge
+    def initialize(graph, from, to, dir = true, attrs = {} )
+      @graph      = graph
+      @from       = from
+      @to         = to
+      @attributes = attrs
+      @directed   = dir
+      @label      = nil
+    end
+    
+    # Converts the edge to dot format.
+    def to_dot
+      dirstring = " -- " 
+      if directed 
+        dirstring = " -> "
+      end
+      
+      return @from.to_dot_id + dirstring + @to.to_dot_id + " \n  "
+    end
+    
+  end  
+  
+  # Class that models the whole graph.  
+  class Graph
+    attr_accessor :attributes
+    attr_accessor :nodes
+    attr_accessor :edges
+    attr_accessor :nodeattr
+    attr_accessor :edgeattr
+    
+    
+    def initialize(attrs= {} )
+      @attributes = attrs
+      @nodeattr   = {}
+      @edgeattr   = {}
+      @nodes      = {}
+      @edges      = []
+      @label      = nil
+      @autonode   = 0  
+    end
+    
+    # Adds node to the graph 
+    def new_node(id, label = nil, attr={})
+      newnode             = Node.new(self, id, label, attr)
+      @nodes[newnode.id]  = newnode
+      return newnode 
+    end
+    
+    # Adds a new node if it doesn't exist. Otherwise returns the existing node
+    # if the node id is nil, autogenerate it 
+    def node(id = nil, label = nil, attr={})
+      unless id
+        @autonode += 1 
+        id = "node_#@autonode"
+      end  
+      nod  = @nodes[id]
+      return nod if nod
+      return new_node(id, label, attr) 
+    end  
+    
+    # Adds an edge to the graph
+    def new_edge(from, to, dir = true, attr={})
+      newedge = Edge.new(self, from, to, dir, attr)
+      @edges  << newedge
+      return newedge 
+    end
+    
+    # Adds an edge by ids. Raises an exception if IDs are not found 
+    def edge(from_id, to_id, dir = true, attr = {})
+      from    = @nodes[from_id]
+      to      = @nodes[to_id]
+      raise "No such node #{from_id}" unless from
+      raise "No such node #{to_id}"   unless to
+      return new_edge(from, to, dir, attr)    
+    end
+    
+    # graph to dot graph
+    def to_dot
+      res   = "digraph { \n"
+      grats = Nanograph.attributes_to_dot(@attributes)
+      res << "graph " + grats + "\n" unless grats.empty?
+      nats  = Nanograph.attributes_to_dot(@nodeattr)
+      res << "node " + nats + "\n" unless nats.empty?
+      for k, n in @nodes
+        res << n.to_dot + "\n"
+      end        
+      for e in @edges 
+        res << e.to_dot + "\n"
+      end 
+      res << "\n}\n"
+      return res
+    end
+    
+    def display()
+      # XXX: this is a security risk on multi-user systems.
+      pipe = IO.popen('dot -Tpng -o/tmp/disp.png', 'wt+')
+      pipe.write(self.to_dot)
+      pipe.close 
+      
+      if $?.exitstatus > 0
+        puts self.to_dot
+      else 
+        system('display /tmp/disp.png &')
+      end
+    end
+    
+    
+  end
+
+end
+
+
+if __FILE__ == $0
+  graph     = Nanograph.graph(fontsize: 10, splines: true, overlap: false,
+rankdir:
+"LR")
+  graph.nodeattr = { fontsize: 10, width:0, height:0, shape: :box, style:
+:rounded}
+  graph.node(nil , "Hello, hello") >> 'world' >> '!'
+  graph.node('node_1') >> 'world 2'
+  graph.display
+end
