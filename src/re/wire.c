00eae8b1d021d7127a694c1bcc3ebaca39796aa2
Beoran <beoran@rubyforge.org>
2011-06-14 16:07:05 +0200

    Oh noes, I'm catching the C bug again. :)

--- src/re/wire.c
+++ src/re/wire.c
@@ -9,9 +9,35 @@ struct ReWire_ {
 };
 
 
+static size_t re_strlen(char * str) {
+  int res;   
+  if(!str) return NULL;
+  for(res = 0; (*str) != '\0' ; str++ ) { res++; }
+  return res;  
+};
+
+static char * re_strdup(char * str) {
+  char * res;
+  int ind, len;
+  if(!str) return NULL;
+  len = re_strlen(str);
+  res = re_malloc(len);
+  for(ind = 0; ind < len; ind++) { res[ind] = str[ind]; } 
+  return res;
+}
+
+
+
 ReWire * rewire_init(ReWire * self, char * str, int size, int room) {
-  
-  
+  if (room == 0) // constant string {
+    self->data = str;
+    self->size = re_strlen(str);
+    self->room = room;  
+  } else {
+    self->data = re_strdup(str);
+    self->size = re_strlen(str);
+    self->room = room;
+  }
 };
 
 
