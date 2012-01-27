c063f0e465b52226ca38501c23e3a37257a5fe63
Beoran <beoran@rubyforge.org>
2012-01-27 14:19:37 +0100

    Map elements, and fiddling with a memory handle idea.

---
+++ data/map/tiles_village_1000.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_village_1000" tilewidth="32" tileheight="32">
+ <image source="tiles_village_1000.png" width="480" height="480"/>
+ <tile id="3">
+  <properties>
+   <property name="type" value="water"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="type" value="stair"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="anim" value="1"/>
+   <property name="type" value="water"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="anim" value="1"/>
+   <property name="type" value="water"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="anim" value="1"/>
+   <property name="type" value="water"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="anim" value="-3"/>
+   <property name="type" value="water"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="anim" value="1"/>
+   <property name="type" value="water"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="anim" value="1"/>
+   <property name="type" value="water"/>
+  </properties>
+ </tile>
+ <tile id="107">
+  <properties>
+   <property name="anim" value="1"/>
+   <property name="type" value="water"/>
+  </properties>
+ </tile>
+ <tile id="108">
+  <properties>
+   <property name="anim" value="1"/>
+   <property name="type" value="water"/>
+  </properties>
+ </tile>
+</tileset>
--- src/mem.c
+++ src/mem.c
@@ -44,5 +44,79 @@ void * mem_move(void * dest, void * src, size_t size) {
 }
 
 
+/* pointer handler. */
+typedef uint32_t Handle;
+
+struct Meminfo_;
+typedef struct Meminfo_ Meminfo;
+struct Memlist_;
+typedef struct Memlist_ Memlist;
+
+
+struct Meminfo_ {
+  void      * ptr;
+  int         refcount;
+  uint16_t    offset;
+  uint8_t     usage;
+  uint8_t     type;
+  uint16_t    nextfree;
+};
+
+struct Memlist_ {
+  int        size;
+  Meminfo  * info;
+  uint16_t   nextfree;
+};
+
+#define MEMLIST_SIZE 1024
+
+Memlist * memlist_init(Memlist * self) {
+  int index;
+  self->size = MEMLIST_SIZE;
+  self->info = mem_calloc(self->size, sizeof(Meminfo));
+  if(!self->info) { self->size = 0; return NULL; }
+  self->nextfree = 0;  
+  for(index = 0; index < self->size; index ++) {
+    Meminfo * aid  = self->info + index;
+    aid->ptr       = NULL;
+    aid->refcount  = -1;
+    aid->usage     = 0;
+    aid->type      = 0;
+    aid->nextfree  = index + 1;
+  }
+  return self;
+}
+
+Handle handle_make(uint16_t offset, uint8_t usage, uint8_t type) {
+  return ((uint32_t)offset) + (((uint32_t)usage)<<16) + (((uint32_t)type)<<24);
+}
+
+Handle memlist_add(Memlist * self, void * ptr, uint8_t type) {
+  Meminfo * aid;
+  if(self->nextfree >= self->size) {
+    Meminfo * big = NULL;
+    if(!mem_resize((void **)&self->info, sizeof(Meminfo) * self->size * 2)) {
+      return 0;
+    }
+    self->size = self->size * 2;
+  }  
+  aid = self->info + self->nextfree;
+  aid->ptr       = ptr;
+  aid->usage++;
+  aid->type      = type;
+  aid->refcount  = 1;
+  aid->offset    = self->nextfree;
+  self->nextfree = aid->nextfree;
+  aid->nextfree  = -1; // next is used, so not known.
+  return handle_make(aid->offset, aid->usage, aid->type);
+}
+
+
+
+
+
+
+
+
 
 
