91088f9abe16db5d3c4dafc809daa89aaac6967a
Beoran <beoran@rubyforge.org>
2012-11-21 13:08:24 +0100

    Naturaldocs is nice, but the input is a bit verbose on C...

--- src/inli.c
+++ src/inli.c
@@ -18,7 +18,13 @@
 *
 * Fully initializes a non-NULL intrusive linked list.
 *
-* Returns: self
+* Parameters:
+*   self - Inli to initialize
+*   next - Next Inli list node to link to or NULL
+*   prev - Previous Inli list node to link to or NULL
+*
+* Returns: 
+*   self
 */
 Inli * inli_initall(Inli * self, Inli * next, Inli * prev) {
   if (!self) return NULL;
@@ -28,18 +34,33 @@ Inli * inli_initall(Inli * self, Inli * next, Inli * prev) {
 }
 
 /**
+* Function: inli_init
+*
 * Initializes the intrusive linked list. Next and prev are set to NULL.
 *
-* Returns: self
+* Parameters:
+*   self - Inli to initialize
+*
+* Returns: 
+*   self
 */
 Inli * inli_init(Inli * self) {
   return inli_initall(self, NULL, NULL);
 }
 
-/** Removes self from the list it is part of. 
-* Does NOT clean up any data asssociated with 
-* the container of the intrusive list and also doesn't free self,
-* since it should be part of the container of the intrusive list. 
+/** 
+* Function: inli_remove
+*
+* Removes self from the list it is part of. 
+* Does NOT clean up any data asssociated with the container of the intrusive 
+* list and also doesn't free self, since self should be part of the container 
+* of the intrusive list. 
+*
+* Parameters:
+*   self - Inli to remove from whole of list
+*
+* Returns: 
+*   self
 */
 Inli * inli_remove(Inli * self) {  
   if(!self) return NULL;
@@ -48,8 +69,17 @@ Inli * inli_remove(Inli * self) {
   return self;
 }
 
-/** Appends other after self. Returns other, or null if either self or 
-other are NULL.  
+/** 
+* Function: inli_add
+*
+* Appends other after self. 
+*
+* Parameters:
+*   self - Inli to append to
+*   other - Inli to append to self.
+*
+* Returns:
+*   other if all went OK, NULL on error
 */
 Inli * inli_add(Inli * self, Inli * other) {  
   if(!self || !other) return NULL;
@@ -58,20 +88,44 @@ Inli * inli_add(Inli * self, Inli * other) {
   return other;
 }
 
-/** Returns the next element in the list. */
+/** 
+* Function: inli_next
+*
+* Parameters:
+*   self - Inli
+*
+* Returns: 
+*   the next element in the list, or NULL if no next item. 
+*/
 Inli * inli_next(Inli * self) {
   if(!self) return NULL;
   return self->next;
 }
 
-/** Returns the previous element in the list. */
+/** 
+* Function: inli_next
+*
+* Parameters:
+*   self - Inli
+*
+* Returns: 
+*   the next previous in the list, or NULL if no previous item. 
+*/
 Inli * inli_prev(Inli * self) {
   if(!self) return NULL;
   return self->prev;  
 }
 
-/** Returns the data for this node of the singly linked list. 
-* Use offsetof to calculate offset.
+/** 
+* Function: inli_data
+* 
+* Parameters:
+* self - list node to get the data of.
+* offset - Use offsetof to calculate this offset.
+*
+* Returns:  
+*   the data for this node of the singly linked list. 
+* 
 */
 void * inli_data(Inli * self, int offset) {
   return (void *)((char *)self - offset);
