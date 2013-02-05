b4d98eb74c87d81d64840e14916dc9eb576f9762
Beoran <beoran@beoran.net>
2013-02-05 23:01:23 +0100

    Sprite subsystem.

--- src/sprite.c
+++ src/sprite.c
@@ -2,6 +2,9 @@
 #include "sprite.h"
 #include "rebox.h"
 
+
+
+
 /* A SpriteFrame is a single frame of animation of a sprite. 
  * It consists of a bounds rectangle, a image pointer, an index in the 
  * animation, and a duration in ms of the frame. The flags field 
@@ -15,12 +18,50 @@ struct SpriteFrame_ {
   int           flags;
 };
 
+static SpriteFrame 
+SpriteFrameZero = { 0 };
+
+/* Get Tarray interface, generate for SpriteFrame type */
+#define TEMPLATE_ZERO   SpriteFrameZero
+#define TEMPLATE_T      SpriteFrame
+#define TEMPLATE_NAME   SpriteFrameArray
+#define TEMPLATE_PREFIX spriteframearray_
+#include "tarray.h"
+
+/* Get Tarray implementation, generate for SpriteFrame type */
+#define TEMPLATE_ZERO   SpriteFrameZero
+#define TEMPLATE_IMPLEMENT
+#define TEMPLATE_T      SpriteFrame
+#define TEMPLATE_NAME   SpriteFrameArray
+#define TEMPLATE_PREFIX spriteframearray_
+#include "tarray.h"
+
+
 
 struct SpriteAction_ {
   int index;
   int flags;
-  
+  SpriteFrameArray frames;
 };
+
+
+static SpriteAction 
+SpriteActionZero = { 0 };
+
+/* Get Tarray interface, generate for SpriteFrame type */
+#define TEMPLATE_ZERO   SpriteFrameZero
+#define TEMPLATE_T      SpriteFrame
+#define TEMPLATE_NAME   SpriteFrameArray
+#define TEMPLATE_PREFIX spriteframearray_
+#include "tarray.h"
+
+/* Get Tarray implementation, generate for SpriteFrame type */
+#define TEMPLATE_ZERO   SpriteFrameZero
+#define TEMPLATE_IMPLEMENT
+#define TEMPLATE_T      SpriteFrame
+#define TEMPLATE_NAME   SpriteFrameArray
+#define TEMPLATE_PREFIX spriteframearray_
+#include "tarray.h"
 
 
 struct Sprite_ {
